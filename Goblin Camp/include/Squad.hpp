e69934b2840f656b0014fe6c4e91e09edc8ff290
Generic Container <ilkka.halila@gmail.com>
2010-06-27 19:41:39 +0300

    Added Squad.cpp/.hpp, begun implementation.

---
+++ Goblin Camp/include/Squad.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <boost/weak_ptr.hpp>
+#include <string>
+#include <list>
+
+#include "Coordinate.hpp"
+#include "Entity.hpp"
+
+enum Orders {
+	GUARD,
+	PATROL,
+	ESCORT
+}
+
+class Squad {
+private:
+	std::string name;
+	int memberReq;
+	//List of NPC uid's
+	std::list<int> members;
+	Orders order;
+	Coordinate targetCoordinate;
+	boost::weak_ptr<Entity> targetEntity;
+	int priority;
+public:
+	Squad(std::string name="Noname nancyboys", int members=0, int priority=0);
+	//Recruits one member if needed and returns true if the squad still requires more members
+	bool UpdateMembers();
+	Orders Order();
+	void Order(Orders);
+	Coordinate TargetCoordinate();
+	void TargetCoordinate(Coordinate);
+	boost::weak_ptr<Entity> TargetEntity();
+	void TargetEntity(boost::weak_ptr<Entity>);
+};
---
+++ Goblin Camp/src/Squad.cpp
@@ -0,0 +1,28 @@
+#include "Squad.hpp"
+#include "Game.hpp"
+
+Squad::Squad(std::string nameValue, int memberValue, int pri) :
+	name(nameValue),
+	memberReq(memberValue),
+	priority(pri)
+	{}
+
+bool Squad::UpdateMembers() {
+	int newMember = Game::Inst()->FindMilitaryRecruit();
+	if (newMember >= 0) { 
+		members.push_back(newMember);
+		Game::Inst()->npcList[newMember]->Squad(shared_from_this());
+	}
+	if (members.size() < memberReq) return true;
+	return false;
+}
+
+Orders Squad::Order() {return order;}
+
+void Squad::Order(Orders newOrder) {order = newOrder;}
+
+Coordinate Squad::TargetCoordinate() {return targetCoordinate;}
+void Squad::TargetCoordinate(Coordinate newTarget) {targetCoordinate = newTarget;}
+
+boost::weak_ptr<Entity> Squad::TargetEntity() {return targetEntity;}
+void Squad::TargetEntity(boost::weak_ptr<Entity> newEntity) {targetEntity = newEntity;}
