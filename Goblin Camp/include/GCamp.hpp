10b93f255e725abe6d669ad94a37fb8e6b98d373
Generic Container <ilkka.halila@gmail.com>
2010-07-06 16:25:07 +0300

    Added license notice to source files, and COPYING.txt

--- Goblin Camp/include/GCamp.hpp
+++ Goblin Camp/include/GCamp.hpp
@@ -1,5 +1,19 @@
-#ifndef GCAMP_HEADER
-#define GCAMP_HEADER
+/* Copyright 2010 Ilkka Halila
+This file is part of Goblin Camp.
+
+Goblin Camp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goblin Camp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+#pragma once
 
 #include <boost/multi_array.hpp>
 #include <boost/thread/thread.hpp>
@@ -11,4 +25,3 @@
 
 void mainLoop();
 int distance(int,int,int,int);
-#endif
--- Goblin Camp/include/Water.hpp
+++ Goblin Camp/include/Water.hpp
@@ -1,5 +1,19 @@
-#ifndef WATER_HEADER
-#define WATER_HEADER
+/* Copyright 2010 Ilkka Halila
+This file is part of Goblin Camp.
+
+Goblin Camp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goblin Camp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+#pragma once
 
 #include <libtcod.hpp>
 #include <boost/enable_shared_from_this.hpp>
@@ -29,5 +43,3 @@ class WaterNode : public boost::enable_shared_from_this<WaterNode> {
 		void UpdateGraphic();
 		Coordinate Position();
 };
-
-#endif
