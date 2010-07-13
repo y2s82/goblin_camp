ab5fceaddad763864a0f15b9eeb5fea8644755ef
Generic Container <ilkka.halila@gmail.com>
2010-07-13 21:05:40 +0300

    Dismantling is now an expert job

--- Goblin Camp/src/Construction.cpp
+++ Goblin Camp/src/Construction.cpp
@@ -439,7 +439,7 @@ void Construction::Dismantle() {
 			jobList.clear();
 		}
 
-		boost::shared_ptr<Job> dismantleJob(new Job((boost::format("Dismantle %s") % name).str()));
+		boost::shared_ptr<Job> dismantleJob(new Job((boost::format("Dismantle %s") % name).str(), MED, 0, false));
 		dismantleJob->ConnectToEntity(shared_from_this());
 		dismantleJob->tasks.push_back(Task(MOVEADJACENT, Position(), shared_from_this()));
 		dismantleJob->tasks.push_back(Task(DISMANTLE, Position(), shared_from_this()));
