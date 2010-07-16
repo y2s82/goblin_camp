4ac44bb4a1b0acfd939f21f3ea3e9f91003e71c2
PiotrLegnica <piotrlegnica@piotrl.pl>
2010-07-16 21:55:28 +0200

    Updated Boost.Build and Boost.Jam to much more recent versions.

--- build/boost-build/jam_src/output.h
+++ build/boost-build/jam_src/output.h
@@ -1,4 +1,4 @@
-/* 
+/*
     Copyright 2007 Rene Rivera
     Distributed under the Boost Software License, Version 1.0.
     (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
@@ -6,6 +6,8 @@
 
 #ifndef BJAM_OUTPUT_H
 #define BJAM_OUTPUT_H
+
+#include <time.h>
 
 #define EXIT_OK 0
 #define EXIT_FAIL 1
@@ -19,5 +21,9 @@ void out_action(
     const char * err_data,
     int exit_reason
     );
+
+char * outf_int( int value );
+char * outf_double( double value );
+char * outf_time( time_t value );
 
 #endif
