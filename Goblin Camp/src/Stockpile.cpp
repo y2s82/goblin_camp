1cfd97b6ab6c235a136ee1700ddf6ada94aab174
Generic Container <ilkka.halila@gmail.com>
2010-07-27 21:50:05 +0300

    Added visual cue to constructions ordered to be dismantled, closes issue 80

--- Goblin Camp/src/Stockpile.cpp
+++ Goblin Camp/src/Stockpile.cpp
@@ -181,13 +181,14 @@ void Stockpile::Draw(Coordinate upleft, TCODConsole* console) {
 				screeny = y - upleft.Y();
 				if (screenx >= 0 && screenx < console->getWidth() && screeny >= 0 &&
 					screeny < console->getHeight()) {
+						if (dismantle) console->setBack(screenx,screeny, TCODColor::darkGrey);
 						console->setFore(screenx, screeny, TCODColor::white);
-						console->setChar(screenx,	screeny, (graphic[1]));
+						console->setChar(screenx, screeny, (graphic[1]));
 
 						if (!containers[Coordinate(x,y)]->empty()) {
 							boost::weak_ptr<Item> item = *containers[Coordinate(x,y)]->begin();
 							if (item.lock()) {
-								console->putCharEx(screenx, screeny, item.lock()->Graphic(), item.lock()->Color(), TCODColor::black);
+								console->putCharEx(screenx, screeny, item.lock()->Graphic(), item.lock()->Color(), dismantle ? TCODColor::darkGrey : TCODColor::black);
 							}
 						}
 				}
