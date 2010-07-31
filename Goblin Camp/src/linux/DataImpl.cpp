2b3372460dfe3f049a8d5e08aabe4da17e3d9071
PiotrLegnica <piotrlegnica@piotrl.pl>
2010-07-31 14:07:38 +0200

    Merged local r316:4641922a63df with gencontain's r349:1d5d4f2e66df (r349 in parent repo).

---
+++ Goblin Camp/src/linux/DataImpl.cpp
@@ -0,0 +1,41 @@
+/* Copyright 2010 Ilkka Halila
+This file is part of Goblin Camp.
+
+Goblin Camp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goblin Camp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+#include "stdafx.hpp"
+
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <boost/filesystem.hpp>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+
+namespace fs = boost::filesystem;
+
+void _ImplFindPersonalDirectory(std::string& dir) {
+	dir = getenv("HOME");
+	dir += "/.goblincamp";
+}
+
+void _ImplFindExecutableDirectory(fs::path& exec, fs::path& execDir, fs::path& dataDir) {
+	char buffer[1024];
+	ssize_t pos = readlink("/proc/self/exe", buffer, 1023);
+	buffer[pos] = '\0';
+	
+	exec    = fs::path(std::string(buffer));
+	execDir = exec.parent_path();
+	dataDir = fs::path(execDir.parent_path()) / "share/goblin-camp/";
+}
