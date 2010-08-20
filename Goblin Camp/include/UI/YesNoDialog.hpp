1285e07f5626370f8cfcb3e76dc960c0d24655cd
Generic Container <ilkka.halila@gmail.com>
2010-08-20 09:04:02 +0300

    Added internal flag to items, and made them update their position on the map accordingly

---
+++ Goblin Camp/include/UI/YesNoDialog.hpp
@@ -0,0 +1,33 @@
+/* Copyright 2010 Ilkka Halila
+ This file is part of Goblin Camp.
+ 
+ Goblin Camp is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Goblin Camp is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License 
+ along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/weak_ptr.hpp>
+#include <libtcod.hpp>
+
+#include "UIComponents.hpp"
+
+class YesNoDialog {
+public:
+    static void ShowYesNoDialog(std::string text, boost::function<void()> leftAction, boost::function<void()> rightAction,
+                            std::string leftButton = "Yes", std::string rightButton = "No");
+};
+
