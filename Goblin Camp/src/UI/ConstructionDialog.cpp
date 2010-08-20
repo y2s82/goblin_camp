1bf9575a71a2200a87acbdcc99298dd0124bfd8a
preevs <preevs@localhost>
2010-08-19 20:40:02 -0400

    incorporate tvStatic's patch for issue #39 (translated to new UI framework)

--- Goblin Camp/src/UI/ConstructionDialog.cpp
+++ Goblin Camp/src/UI/ConstructionDialog.cpp
@@ -44,7 +44,9 @@ Dialog* ConstructionDialog::ConstructionInfoDialog(Construction* cons) {
         dialog->AddComponent(new Button("Dismantle", boost::bind(&ConstructionDialog::Dismantle, dialog), 28, 1, 13));
         if(cons->HasTag(STOCKPILE)) {
             constructionInfoDialog->SetHeight(40);
-            dialog->AddComponent(new ScrollPanel(2, 5, 46, 34,
+			dialog->AddComponent(new Button("All", boost::bind(&Stockpile::SetAllAllowed, static_cast<Stockpile *>(cons), true), 2, 5, 8));
+			dialog->AddComponent(new Button("None", boost::bind(&Stockpile::SetAllAllowed, static_cast<Stockpile *>(cons), false), 11, 5, 8));
+            dialog->AddComponent(new ScrollPanel(2, 8, 46, 31,
 								 new UIList<ItemCat>(&Item::Categories, 0, 0, 46, Item::Categories.size(),
                                                    boost::bind(&ConstructionDialog::DrawCategory, cons, _1, _2, _3, _4, _5, _6, _7),
                                                    boost::bind(&Stockpile::SwitchAllowed, static_cast<Stockpile *>(cons), _1, boost::bind(&UI::ShiftPressed, UI::Inst()))), false));
