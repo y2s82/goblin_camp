a0af04b6ec1070a63a767c7f1eea17c0ac0ecc79
Generic Container <ilkka.halila@gmail.com>
2010-10-25 15:35:11 +0300

    Tweaked configuration to make GC build with msvc10

--- Goblin Camp/src/KuhnMunkres.cpp
+++ Goblin Camp/src/KuhnMunkres.cpp
@@ -14,6 +14,7 @@
  You should have received a copy of the GNU General Public License 
  along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
 
+#include "stdafx.hpp"
 #include "KuhnMunkres.hpp"
 
 std::vector<int> FindBestMatching(boost::numeric::ublas::matrix<int> costs) {
