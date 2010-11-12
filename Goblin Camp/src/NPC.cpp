40534d78768f567cae17c733bdf2bb7ff5389283
Generic Container <ilkka.halila@gmail.com>
2010-11-12 22:56:00 +0200

    Further tweaked hunger value

--- Goblin Camp/src/NPC.cpp
+++ Goblin Camp/src/NPC.cpp
@@ -295,7 +295,7 @@ void NPC::Update() {
 		} else if (thirst > THIRST_THRESHOLD * 2) Kill();
 		if (hunger > HUNGER_THRESHOLD && (rand() % (UPDATES_PER_SECOND*5)) == 0) {
 			HandleHunger();
-		} else if (hunger > HUNGER_THRESHOLD * 3) Kill();
+		} else if (hunger > HUNGER_THRESHOLD * 2) Kill();
 	}
 
 	if (needsSleep) {
