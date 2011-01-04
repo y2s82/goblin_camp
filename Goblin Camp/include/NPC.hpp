18473f44a91926e055f7e05ef380afadb6f022f9
Immortius <Immortius@localhost>
2011-01-04 13:29:39 +1100

    - Updated to libtcod r522

---
+++ Goblin Camp/include/NPC.hpp
@@ -0,0 +1,260 @@
+/* Copyright 2010 Ilkka Halila
+This file is part of Goblin Camp.
+
+Goblin Camp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goblin Camp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+#pragma once
+
+#include <queue>
+#include <list>
+
+#include <boost/serialization/split_member.hpp>
+
+#include <boost/thread/thread.hpp>
+#include <boost/multi_array.hpp>
+#include <boost/function.hpp>
+#include <boost/weak_ptr.hpp>
+
+#include <libtcod.hpp>
+
+#include "Coordinate.hpp"
+#include "Job.hpp"
+#include "Entity.hpp"
+#include "Container.hpp"
+#include "StatusEffect.hpp"
+#include "Squad.hpp"
+#include "Attack.hpp"
+
+#define LOS_DISTANCE 12
+#define MAXIMUM_JOB_ATTEMPTS 5
+
+#define THIRST_THRESHOLD (UPDATES_PER_SECOND * 60 * 10)
+#define HUNGER_THRESHOLD 18000
+#define WEARY_THRESHOLD (UPDATES_PER_SECOND * 60 * 12)
+#define DRINKABLE_WATER_DEPTH 2
+#define WALKABLE_WATER_DEPTH 1
+
+typedef int NPCType;
+
+enum AiThink {
+	AINOTHING,
+	AIMOVE
+};
+
+enum Skill {
+	MASONRY,
+	CARPENTRY,
+	SKILLAMOUNT
+};
+
+class SkillSet {
+	friend class boost::serialization::access;
+private:
+	template<class Archive>
+	void save(Archive & ar, const unsigned int version) const;
+	template<class Archive>
+	void load(Archive & ar, const unsigned int version);
+	BOOST_SERIALIZATION_SPLIT_MEMBER()
+
+	int skills[SKILLAMOUNT];
+public:
+	SkillSet();
+	int operator()(Skill);
+	void operator()(Skill, int);
+};
+
+struct NPCPreset {
+	NPCPreset(std::string);
+	std::string typeName;
+	std::string name;
+	std::string plural;
+	TCODColor color;
+	int graphic;
+	bool expert;
+	int health;
+	std::string ai;
+	bool needsNutrition;
+	bool needsSleep;
+	bool generateName;
+	int stats[STAT_COUNT];
+	int resistances[RES_COUNT];
+	bool spawnAsGroup;
+	TCOD_dice_t group;
+	std::list<Attack> attacks;
+	std::set<std::string> tags;
+	int tier;
+	ItemType deathItem;
+};
+
+class NPC : public Entity {
+	friend class boost::serialization::access;
+	friend class Game;
+	friend class NPCListener;
+
+private:
+	template<class Archive>
+	void save(Archive & ar, const unsigned int version) const;
+	template<class Archive>
+	void load(Archive & ar, const unsigned int version);
+	BOOST_SERIALIZATION_SPLIT_MEMBER()
+
+	NPC(Coordinate = Coordinate(0,0),
+		boost::function<bool(boost::shared_ptr<NPC>)> findJob = boost::function<bool(boost::shared_ptr<NPC>)>(),
+		boost::function<void(boost::shared_ptr<NPC>)> react = boost::function<void(boost::shared_ptr<NPC>)>());
+	NPCType type;
+	int timeCount;
+	std::deque<boost::shared_ptr<Job> > jobs;
+	int taskIndex;
+	int orderIndex;
+	boost::mutex pathMutex;
+	TCODPath *path;
+	int pathIndex;
+	bool nopath;
+	bool findPathWorking;
+	int timer;
+	unsigned int nextMove;
+	TaskResult lastMoveResult;
+	bool run;
+	TCODColor _color, _bgcolor;
+	int _graphic;
+	bool taskBegun;
+	bool expert;
+
+	boost::weak_ptr<Item> carried;
+	boost::weak_ptr<Item> mainHand;
+	boost::weak_ptr<Item> offHand;
+	boost::weak_ptr<Item> armor;
+	boost::weak_ptr<Container> quiver;
+
+	int thirst, hunger, weariness;
+	int thinkSpeed;
+	std::list<StatusEffect> statusEffects;
+	std::list<StatusEffect>::iterator statusEffectIterator;
+	int statusGraphicCounter;
+	void HandleThirst();
+	void HandleHunger();
+	void HandleWeariness();
+	int health, maxHealth;
+	boost::weak_ptr<Item> foundItem;
+	boost::shared_ptr<Container> inventory;
+
+	std::list<boost::weak_ptr<NPC> >nearNpcs;
+	bool needsNutrition;
+	bool needsSleep;
+	bool hasHands;
+	bool isTunneler;
+
+	boost::function<bool(boost::shared_ptr<NPC>)> FindJob;
+	boost::function<void(boost::shared_ptr<NPC>)> React;
+
+	int baseStats[STAT_COUNT];
+	int effectiveStats[STAT_COUNT];
+	int baseResistances[RES_COUNT];
+	int effectiveResistances[RES_COUNT];
+	bool aggressive, coward;
+	boost::weak_ptr<NPC> aggressor;
+	bool dead;
+	boost::weak_ptr<Squad> squad;
+
+	std::list<Attack> attacks;
+
+	bool escaped;
+	bool Escaped();
+	void Escape();
+	void DestroyAllItems();
+	void UpdateStatusEffects();
+
+	static std::map<std::string, NPCType> NPCTypeNames;
+
+	void UpdateVelocity();
+	int addedTasksToCurrentJob;
+
+public:
+	~NPC();
+	SkillSet Skills;
+	AiThink Think();
+	void Update();
+	void Draw(Coordinate, TCODConsole*);
+	virtual void GetTooltip(int x, int y, Tooltip *tooltip);
+	void Position(Coordinate,bool);
+	virtual void Position(Coordinate);
+	virtual Coordinate Position();
+	void speed(unsigned int);
+	unsigned int speed();
+	void color(TCODColor,TCODColor=TCODColor::black);
+	void graphic(int);
+
+	Task* currentTask();
+	Task* nextTask();
+	boost::weak_ptr<Job> currentJob();
+	Coordinate currentTarget();
+	boost::weak_ptr<Entity> currentEntity();
+	void TaskFinished(TaskResult, std::string = "");
+	TaskResult Move(TaskResult);
+	void findPath(Coordinate);
+	void StartJob(boost::shared_ptr<Job>);
+	void AddEffect(StatusEffectType);
+	void AddEffect(StatusEffect);
+	void RemoveEffect(StatusEffectType);
+	bool HasEffect(StatusEffectType);
+	std::list<StatusEffect>* StatusEffects();
+	void AbortCurrentJob(bool);
+	void AbortJob(boost::weak_ptr<Job>);
+
+	bool Expert();
+	void Expert(bool);
+
+	bool Dead();
+	void Kill();
+	void PickupItem(boost::weak_ptr<Item>);
+	void DropItem(boost::weak_ptr<Item>);
+	void Hit(boost::weak_ptr<Entity>, bool careful = false);
+	void FireProjectile(boost::weak_ptr<Entity>);
+	void Damage(Attack*, boost::weak_ptr<NPC> aggr = boost::weak_ptr<NPC>());
+
+	void MemberOf(boost::weak_ptr<Squad>);
+	boost::weak_ptr<Squad> MemberOf();
+	void GetMainHandAttack(Attack&);
+	bool WieldingRangedWeapon();
+	void FindNewWeapon();
+	boost::weak_ptr<Item> Wielding();
+	bool HasHands();
+	bool IsTunneler();
+	void FindNewArmor();
+	boost::weak_ptr<Item> Wearing();
+
+	int GetHealth();
+	int GetMaxHealth();
+
+	static void LoadPresets(std::string);
+	static std::vector<NPCPreset> Presets;
+	static std::string NPCTypeToString(NPCType);
+	static NPCType StringToNPCType(std::string);
+	int GetNPCSymbol();
+
+	void InitializeAIFunctions();
+
+	static bool GetSquadJob(boost::shared_ptr<NPC>);
+	static bool JobManagerFinder(boost::shared_ptr<NPC>);
+	static void PlayerNPCReact(boost::shared_ptr<NPC>);
+	static void PeacefulAnimalReact(boost::shared_ptr<NPC>);
+	static bool PeacefulAnimalFindJob(boost::shared_ptr<NPC>);
+	static void HostileAnimalReact(boost::shared_ptr<NPC>);
+	static bool HostileAnimalFindJob(boost::shared_ptr<NPC>);
+	static bool HungryAnimalFindJob(boost::shared_ptr<NPC>);
+
+	static unsigned int pathingThreadCount;
+	static boost::mutex threadCountMutex;
+};
+
+void tFindPath(TCODPath*, int, int, int, int, boost::mutex*, bool*, bool*, bool);
---
+++ Goblin Camp/src/Squad.cpp
@@ -0,0 +1,142 @@
+/* Copyright 2010 Ilkka Halila
+This file is part of Goblin Camp.
+
+Goblin Camp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goblin Camp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+#include "stdafx.hpp"
+
+#ifdef DEBUG
+#include <iostream>
+#endif
+
+#include "Squad.hpp"
+#include "Game.hpp"
+
+Squad::Squad(std::string nameValue, int memberValue, int pri) :
+	name(nameValue),
+	memberReq(memberValue),
+	generalOrder(NOORDER),
+	orders(std::vector<Order>()),
+	targetCoordinates(std::vector<Coordinate>()),
+	targetEntities(std::vector<boost::weak_ptr<Entity> >()),
+	priority(pri),
+	weapon(-1),
+	armor(-1)
+{}
+
+Squad::~Squad() {
+#ifdef DEBUG
+	std::cout<<"Squad "<<name<<" destructed.\n";
+#endif
+}
+
+bool Squad::UpdateMembers() {
+	if ((signed int)members.size() < memberReq) {
+		int newMember = Game::Inst()->FindMilitaryRecruit();
+		if (newMember >= 0) { 
+			members.push_back(newMember);
+			Game::Inst()->npcList[newMember]->MemberOf(shared_from_this());
+		}
+	} else if ((signed int)members.size() > memberReq) {
+		Game::Inst()->npcList[members.back()]->MemberOf(boost::weak_ptr<Squad>());
+		members.pop_back();
+	}
+
+	if ((signed int)members.size() < memberReq) return true;
+	return false;
+}
+
+Order Squad::GetOrder(int &orderIndex) {
+	++orderIndex;
+	if (orderIndex < 0 || orderIndex >= orders.size())
+		orderIndex = 0;
+	return orders.empty() ? NOORDER : orders[orderIndex];
+}
+
+//Setting an order resets the target of the order
+void Squad::AddOrder(Order newOrder) {
+	orders.push_back(newOrder);
+	targetCoordinates.push_back(Coordinate(-1,-1));
+	targetEntities.push_back(boost::weak_ptr<Entity>());
+}
+
+void Squad::ClearOrders() {
+	orders.clear();
+	targetCoordinates.clear();
+	targetEntities.clear();
+	generalOrder = NOORDER;
+}
+
+Coordinate Squad::TargetCoordinate(int index) {
+	if (index >= 0 && index < targetCoordinates.size()) {
+		return targetCoordinates[index];
+	} else return Coordinate(-1,-1);
+}
+void Squad::AddTargetCoordinate(Coordinate newTarget) {targetCoordinates.back() = newTarget;}
+
+boost::weak_ptr<Entity> Squad::TargetEntity(int index) {
+	if (index >= 0 && index < targetEntities.size()) {
+		return targetEntities[index];
+	} else return boost::weak_ptr<Entity>();
+}
+
+void Squad::AddTargetEntity(boost::weak_ptr<Entity> newEntity) {targetEntities.back() = newEntity;}
+
+int Squad::MemberCount() { return members.size(); }
+int Squad::MemberLimit() { return memberReq; }
+void Squad::MemberLimit(int val) { memberReq = val; }
+std::string Squad::Name() { return name; }
+void Squad::Name(std::string value) { name = value; }
+
+void Squad::Leave(int member) {
+	for (std::list<int>::iterator membi = members.begin(); membi != members.end(); ++membi) {
+		if (*membi == member) {
+			members.erase(membi);
+			break;
+		}
+	}
+}
+
+void Squad::Priority(int value) { priority = value; }
+int Squad::Priority() { return priority; }
+
+void Squad::RemoveAllMembers() {
+	for (std::list<int>::iterator membi = members.begin(); membi != members.end(); ++membi) {
+		Game::Inst()->npcList[*membi]->MemberOf(boost::weak_ptr<Squad>());
+	}
+	members.clear();
+}
+
+ItemCategory Squad::Weapon() { return weapon; }
+void Squad::Weapon(ItemCategory value) { weapon = value; }
+
+void Squad::Rearm() {
+	for (std::list<int>::iterator memberi = members.begin(); memberi != members.end(); ++memberi) {
+		Game::Inst()->npcList[*memberi]->FindNewWeapon();
+	}
+}
+
+ItemCategory Squad::Armor() { return armor; }
+void Squad::Armor(ItemCategory value) { armor = value; }
+
+void Squad::Reequip() {
+	for (std::list<int>::iterator memberi = members.begin(); memberi != members.end(); ++memberi) {
+		Game::Inst()->npcList[*memberi]->FindNewArmor();
+	}
+}
+
+Order Squad::GetGeneralOrder() { 
+	return generalOrder;
+}
+
+void Squad::SetGeneralOrder(Order order) { generalOrder = order; }
