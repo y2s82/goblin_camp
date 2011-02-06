861f106420fa21facf6d1e326160a5253e79f36f
Generic Container <ilkka.halila@gmail.com>
2011-02-06 23:16:25 +0200

    Removed unused code

--- Goblin Camp/src/Game.cpp
+++ Goblin Camp/src/Game.cpp
@@ -1582,8 +1582,6 @@ int Game::CharWidth() const { return charWidth; }
 
 void Game::RemoveNPC(boost::weak_ptr<NPC> wnpc) {
 	if (boost::shared_ptr<NPC> npc = wnpc.lock()) {
-		if (boost::iequals(npc->name, "orc")) --orcCount;
-		else if (boost::iequals(npc->name, "goblin")) --goblinCount;
 		npcList.erase(npc->uid);
 	}
 }
