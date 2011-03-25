3f0e9eacac754f9d73277c5aff2fe7e5f22c54db
Generic Container <ilkka.halila@gmail.com>
2011-03-25 15:51:51 +0200

    Added virtual destructors to Drawable and Scrollable, added destructor to ScrollPanel that properly deletes its contents. Fixes constructions not getting removed properly due to memory leakage

--- Goblin Camp/include/UI/UIComponents.hpp
+++ Goblin Camp/include/UI/UIComponents.hpp
@@ -1,18 +1,18 @@
 /* Copyright 2010-2011 Ilkka Halila
- This file is part of Goblin Camp.
- 
- Goblin Camp is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- Goblin Camp is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License 
- along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+This file is part of Goblin Camp.
+
+Goblin Camp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goblin Camp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
 #pragma once
 
 #include <string>
@@ -39,10 +39,12 @@ protected:
 	boost::function<void(int, int, Tooltip*)> getTooltip;
 public:
 	Drawable(int x, int y, int nwidth, int nheight):
-		_x(x), _y(y), width(nwidth), height(nheight), visible(0), getTooltip(0) {}
+	  _x(x), _y(y), width(nwidth), height(nheight), visible(0), getTooltip(0) {}
+	virtual ~Drawable() {}
+
 	virtual void Draw(int, int, TCODConsole *) = 0;
 	virtual MenuResult Update(int x, int y, bool clicked, TCOD_key_t key)
-		{return (x >= _x && x < _x + height && y >= _y && y < _y + height) ? MENUHIT : NOMENUHIT;}
+	{return (x >= _x && x < _x + height && y >= _y && y < _y + height) ? MENUHIT : NOMENUHIT;}
 	int Height() { return height; }
 	bool Visible() { return !visible || visible(); }
 	void SetVisible(boost::function<bool()> nvisible) { visible = nvisible; }
@@ -52,6 +54,7 @@ public:
 
 class Scrollable {
 public:
+	virtual ~Scrollable() {}
 	virtual void Draw(int x, int y, int scroll, int width, int height, TCODConsole *) = 0;
 	virtual int TotalHeight() = 0;
 	virtual MenuResult Update(int x, int y, bool clicked, TCOD_key_t key) { return NOMENUHIT; }
@@ -63,7 +66,7 @@ protected:
 	std::vector<Drawable *> components;
 public:
 	UIContainer(std::vector<Drawable *> ncomponents, int nx, int ny, int nwidth, int nheight):
-		Drawable(nx, ny, nwidth, nheight), components(ncomponents) {}
+	  Drawable(nx, ny, nwidth, nheight), components(ncomponents) {}
 	~UIContainer();
 	void AddComponent(Drawable *component);
 	virtual void Draw(int, int, TCODConsole *);
@@ -74,7 +77,7 @@ public:
 class Panel: public Drawable {
 public:
 	Panel(int nwidth, int nheight):
-	Drawable(0, 0, nwidth, nheight) {}
+	  Drawable(0, 0, nwidth, nheight) {}
 	void ShowModal();
 	virtual void Open();
 	virtual void Close();
