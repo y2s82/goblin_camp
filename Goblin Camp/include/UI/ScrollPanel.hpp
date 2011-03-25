6826b220c9deacceee4a9f9709bad1cff2f39414
Generic Container <ilkka.halila@gmail.com>
2011-03-25 15:51:51 +0200

    Added virtual destructors to Drawable and Scrollable, added destructor to ScrollPanel that properly deletes its contents. Fixes constructions not getting removed properly due to memory leakage

--- Goblin Camp/include/UI/ScrollPanel.hpp
+++ Goblin Camp/include/UI/ScrollPanel.hpp
@@ -1,18 +1,18 @@
 /* Copyright 2010-2011 Ilkka Halila
- This file is part of Goblin Camp.
- 
- Goblin Camp is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- Goblin Camp is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License 
- along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
+This file is part of Goblin Camp.
+
+Goblin Camp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Goblin Camp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
 #pragma once
 
 #include <string>
@@ -34,9 +34,10 @@ private:
 public:
 	ScrollPanel(
 		int x, int y, int nwidth, int nheight, Scrollable *ncontents, bool ndrawFrame = true, int nstep = 1
-	):
-		contents(ncontents), scroll(0), scrollBar(0), drawFrame(ndrawFrame),
+		):
+	contents(ncontents), scroll(0), scrollBar(0), drawFrame(ndrawFrame),
 		step(nstep), Drawable(x, y, nwidth, nheight) {}
+	~ScrollPanel() { delete contents; }
 	void Draw(int, int, TCODConsole *);
 	MenuResult Update(int, int, bool, TCOD_key_t);
 	void GetTooltip(int, int, Tooltip *);
