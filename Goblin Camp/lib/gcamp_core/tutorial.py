c9b7fd0163870c2870e3d5c191c5cf276c94764e
Generic Container <ilkka.halila@gmail.com>
2011-04-10 18:06:22 +0300

    Improved tutorial

--- Goblin Camp/lib/gcamp_core/tutorial.py
+++ Goblin Camp/lib/gcamp_core/tutorial.py
@@ -22,6 +22,8 @@ import gcamp.events
 class Tutorial (gcamp.events.EventListener):
 	def __init__(self):
 		self.stage = -1
+		self.stockpiles = 0
+		self.planks = 0
 	
 	def onGameStart(self):
 		if gcamp.config.getCVar("tutorial") == '1':
@@ -30,90 +32,129 @@ class Tutorial (gcamp.events.EventListener):
 			self.stage = -1
 		
 		if self.stage == 0:
-			gcamp.config.setCVar("tutorial", '0')
-			gcamp.delay(100, lambda : gcamp.ui.messageBox("Welcome to the Goblin Camp tutorial!              "+
-						"You're best off by first building a stockpile:    " +
-						"                                                  " +
-						"        -Right-click to open the menu             " +
-						"        -choose Build->Basics->Stockpile          " +
-						"                                                  " +
-						"Then just either click once for each corner of    " +
-						"the area, or click and drag the area.             "))
+			def startMessage():
+				gcamp.config.setCVar("tutorial", '0')
+				gcamp.ui.messageBox(
+						"Welcome to the Goblin Camp tutorial!              "
+						"You're best off by first building 3 stockpiles:   "
+						"                                                  "
+						"        -Right-click to open the menu             "
+						"        -choose Build->Basics->Stockpile          "
+						"                                                  "
+						"Then just either click once for each corner of    "
+						"the area, or click and drag the area.             ")
+			gcamp.delay(100, startMessage)
 
 	def onBuildingCreated(self, construct, x, y):
 		if self.stage == 0 and construct.getTypeString().lower() == "stockpile":
-			gcamp.ui.messageBox("Good work! Now left-click on the stockpile, and   " +
-						"click on the 'All' button to allow all item       " +
-						"categories. Your goblins should get to work       " +
-						"storing everything in there.                      " +
-						"Remember though that setting up more specialised  " +
-						"stockpiles is key to an efficient camp")
+			self.stockpiles = self.stockpiles + 1
+		if self.stage == 0 and self.stockpiles >= 3:
+			def stockpileMessage1():
+				gcamp.ui.messageBox(
+				"Good work! Now you need to set the proper         "
+				"allowances for each stockpile. Left-click on each "
+				"one and allow Wood in one, Food and Seeds in      "
+				"another and Tools in the third. Your goblins      "
+				"should get to work storing all the items.         "
+				"                                                  "
+				"Remember to build new stockpiles and/or change    "
+				"the allowances as you start producing different   "
+				"items.                                            ")
+				gcamp.ui.announce("Stockpile allowances: 1. Wood,  2. Food + Seed,  3. Tool")
+				gcamp.ui.announce("Remember to build more stockpiles / allow other things as needed!")
+				
+			gcamp.delay(125, stockpileMessage1)
 
-			gcamp.delay(400, lambda : gcamp.ui.messageBox( "Next you should do two things:                    " +
-						"                                                  " +
-						"    -Choose Orders->Designate trees and designate " +
-						"trees that can be chopped down for wood.          " +
-						"    -Open the Stockmanager and shift+click on the " +
-						"+-sign underneath 'Wood log' a few times.         " +
-						"                                                  " +
-						"Soon you should see two of your goblins grab the  " +
-						"two axes and go forth to chop some trees down.    " +
-						"The axes will degrade as they're used, and        " +
-						"eventually break, so you'll need a stone quarry   " +
-						"once you're established to be able to keep felling" +
-						"trees.                                            " +
-						"You can also filter the stockmanager by typing in " +
-						"part of an items name or category, allowing you   " +
-						"to quickly find what you need. "))
+			def stockpileMessage2():
+				gcamp.ui.messageBox( 
+						"Next you should do two things:                    "
+						"                                                  "
+						"    -Choose Orders->Designate trees and designate "
+						"trees that can be chopped down for wood.          "
+						"    -Open the Stockmanager and shift+click on the "
+						"+-sign underneath 'Wood log' a few times.         "
+						"                                                  "
+						"Soon you should see two of your goblins grab the  "
+						"two axes and go forth to chop some trees down.    "
+						"                                                  "
+						"The axes will degrade as they're used, and        "
+						"eventually break, so you'll need a stone quarry   "
+						"once you're established to be able to keep felling"
+						"trees.                                            "
+						"                                                  "
+						"You can also filter the stockmanager by typing in "
+						"part of an items name or category, allowing you   "
+						"to quickly find what you need.                    ")
+				gcamp.ui.announce("Designate trees and increase the wood log minimum")
+			gcamp.delay(1000, stockpileMessage2)
 			self.stage = 10
+			
 		elif self.stage == 20 and construct.getTypeString().lower() == "saw pit":
-			gcamp.ui.messageBox("Now order up some planks from the                 " +
-						"Stock Manager, we'll need those for more advanced " + 
-						"constructions.")
+			gcamp.ui.messageBox(
+						"Now order up some planks (50 is a good number)    "
+						"from the Stock Manager, we'll need those for more "
+						"advanced constructions.                           ")
 			self.stage = 30
 			
 	def onItemCreated(self, item, x, y):
 		if self.stage == 10 and item.getTypeString().lower() == "wood log":
-			gcamp.delay(75, lambda : gcamp.ui.messageBox( "Alright, now you have wood logs! Once the logs    " +
-						"have been stored in the stockpile, choose         " +
-						"Build->Workshops->Saw pit.                        " +
-						"Place it somewhere convenient"))
-			gcamp.delay(450, lambda : gcamp.ui.messageBox("Now would be a good time to designate a farmplot. " +
-						"Choose some suitable place, designate it, and     " +
-						"enable planting of all seed types by              " +
-						"left-clicking the farmplot and clicking the boxes " +
-						"in the sidebar.                                   " +
-						"Perhaps consider building a dedicated seed        " +
-						"stockpile somewhere near your farmplots?"))
+			gcamp.ui.messageBox(
+						"Now would be a good time to designate a farm plot."
+						"Choose some suitable place, designate it, and     "
+						"enable planting of all seed types by              "
+						"left-clicking the farm plot and clicking the boxes"
+						"in the sidebar.                                   "
+						"(Build->Basics->Farm plot)")
+			gcamp.ui.announce("Build a farm plot")
+
+			def sawpitMessage():
+				gcamp.ui.messageBox(
+						"Alright, now you have wood logs! Once the logs    "
+						"have been stored in the stockpile, choose         "
+						"Build->Workshops->Saw pit.                        "
+						"Place it somewhere convenient.")
+				gcamp.ui.announce("Build a saw pit")
+				
+			gcamp.delay(1000, sawpitMessage)
 			self.stage = 20
 			
 		elif self.stage == 30 and item.getTypeString().lower() == "wood plank":
-			gcamp.delay(75, lambda : gcamp.ui.messageBox( "Now build a carpenter, it'll allow you to build   " +
-						"crates to store most items and wooden clubs for   " +
-						"your future military. Remember to build buckets,  " +
-						"you'll need them to transport filth to the        " +
-						"spawning pool and water to put out fires"))
-			gcamp.delay(250, lambda : gcamp.ui.messageBox("It's a good idea to check out what your territory " +
-						"looks like time to time.                          " +
-						"        Territory->Toggle territory overlay       " +
-						"It'll expand automatically as you build things,   " +
-						"you just need to remember that goblins will mostly" +
-						"_not_ venture outside your territory to pick items" +
-						"up or otherwise. They will favour a drinking spot " +
-						"inside your territory as well"))
-			gcamp.delay(450, lambda : gcamp.ui.messageBox("This is the end of the tutorial.                  " +
-						"It's a good idea to choose a site for the spawning" +
-						"pool soon (Remember to activate filth and corpse  " +
-						"dumping jobs by right-clicking the spawning pool) " +
-						"Most constructions will have a description in     " +
-						"their tooltip, so look through the menus for what " +
-						"you can build. As you build more, and get more    " +
-						"orcs and goblins from the spawning pool you'll    " +
-						"advance through tiers and gain access to more     " +
-						"advanced buildings. Also don't forget about       " +
-						"defenses. Build a palisade, traps, and  establish " +
-						"some guard squads!"))
+			self.planks = self.planks + 1
+			if (self.planks >= 25):
+				gcamp.delay(75, lambda : gcamp.ui.messageBox(
+							"Now build a carpenter, it'll allow you to build   "
+							"crates to store most items and wooden clubs for   "
+							"your future military. Remember to build buckets,  "
+							"you'll need them to transport filth to the        "
+							"spawning pool and water to put out fires.         "))
+				gcamp.delay(1000, lambda : gcamp.ui.messageBox(
+							"It's a good idea to check out what your territory "
+							"looks like time to time.                          "
+							"                                                  "
+							"        Territory->Toggle territory overlay       "
+							"                                                  "
+							"It'll expand automatically as you build things,   "
+							"you just need to remember that goblins will mostly"
+							"_not_ venture outside your territory to pick items"
+							"up or otherwise. They will favour a drinking spot "
+							"inside your territory as well                     "))
+				gcamp.delay(1750, lambda : gcamp.ui.messageBox(
+							"This is the end of the tutorial.                  "
+							"It's a good idea to choose a site for the spawning"
+							"pool soon.                                        "
+							"(Remember to activate filth and corpse dumping    "
+							"jobs by right-clicking the spawning pool)         "
+							"                                                  "
+							"Most constructions will have a description in     "
+							"their tooltip, so look through the menus for what "
+							"you can build. As you build more, and get more    "
+							"orcs and goblins from the spawning pool you'll    "
+							"advance through tiers and gain access to more     "
+							"advanced buildings.                               "
+							"                                                  "
+							"Also don't forget about defenses. Build a         "
+							"palisade, traps, and  establish some guard squads!"))
 
-			self.stage = 40
+				self.stage = 40
 
 gcamp.events.register(Tutorial())
