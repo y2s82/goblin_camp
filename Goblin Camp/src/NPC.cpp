3b2dc773d1fbe395dfd86937b327d57c466c0a65
Generic Container <ilkka.halila@gmail.com>
2011-07-21 22:36:41 +0300

    Fired projectiles become player faction property automatically

--- Goblin Camp/src/NPC.cpp
+++ Goblin Camp/src/NPC.cpp
@@ -1983,6 +1983,7 @@ void NPC::FireProjectile(boost::weak_ptr<Entity> target) {
 						projectile->PutInContainer();
 						projectile->Position(Position());
 						projectile->CalculateFlightPath(targetEntity->Position(), 100, GetHeight());
+						projectile->SetFaction(PLAYERFACTION);
 					}
 				}
 				break;
