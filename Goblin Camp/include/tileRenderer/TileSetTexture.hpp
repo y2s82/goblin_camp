6d6c410e9040bbccdb8b53ff5ee238424b0dd0f4
gasche <gasche.dycl@gmail.com>
2011-08-05 13:34:04 +0200

    [minor] fix the "/* included in comment" warnings

--- Goblin Camp/include/tileRenderer/TileSetTexture.hpp
+++ Goblin Camp/include/tileRenderer/TileSetTexture.hpp
@@ -21,7 +21,7 @@ along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
 #include "tileRenderer/Corner.hpp"
 
 
-/*******************
+/*******************/
 // TileSetTexture
 //
 // Encapsulates a texture containing equally-sized
